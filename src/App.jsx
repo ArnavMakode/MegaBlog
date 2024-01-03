4f77bb0299734e1ec20f970162048ab718aa67be
ArnavMakode <arnavmakode11@gmail.com>
2024-01-03 14:59:27 +0530

    Configured Redux Store

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
+import authService from "./services/appwrite/auth";
+import { Footer, Header } from "./components";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>MegaBlog</h1>
-    </>
-  )
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) dispatch(login(userData));
+        else dispatch(logout());
+      })
+      .catch((e) => console.log(e))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main></main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
