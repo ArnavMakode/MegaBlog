e893b4d30d798522fa56c7a5138559a4b4291d27
ArnavMakode <arnavmakode11@gmail.com>
2024-01-08 19:24:07 +0530

    Created auth form components

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,11 @@ import Footer from "./Footer/Footer"
 import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
+import Button from "./Button"
+import Input from "./Input"
+import Select from "./Select"
+import Login from "./Login"
+import Signup from "./Signup"
+import PostCard from "./PostCard"
 
-export {Header, Footer, Container, Logo, LogoutBtn}
+export { Button, Input, Select, Header, Footer, Container, Logo, LogoutBtn, Login, Signup, PostCard }
